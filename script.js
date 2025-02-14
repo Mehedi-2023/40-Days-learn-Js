5cd9b70558cab0490a2e96ab0f0b0c644e15d246
Mehedi-2023 <mehedihasanwebd2025@gmail.com>
2025-02-15 02:14:56 +0600

    day_01 commit

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Hello, tapaScript!\n");
+console.log("Welcome to 40 Days of JavaScript!\n");
+document.write("Check the browser console for a message!\n");
+
+// adding text inside HTML element 
+
+document.getElementById("test_1").innerText = 'This is text inside HTML element through the ID selector'
